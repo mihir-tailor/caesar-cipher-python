bba507428cdbaf59139815468a6267513266220c
Mihir Tailor <mihirtailor47@gmail.com>
2025-07-11 12:39:05 -0400

    Added art.py and imported to main.py for Caesar Cipher Art

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # Caesar Cipher Program
 # This program allows users to encode or decode messages using the Caesar cipher technique.
 # The user can specify the shift amount and whether they want to encode or decode the message.
+import art
+
+print(art.logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
